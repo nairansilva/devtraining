c6f40c6f71384a22e7f8ba69f16f395faff37f5d
nairansilva <nairan.asilva@gmail.com>
2022-12-19 00:08:02 -0300

    Criado os scripts para criação das tabelas courses e tags através das migrações

--- src/migrations/1671418046262-CreateCoursesTable.ts
+++ src/migrations/1671418046262-CreateCoursesTable.ts
@@ -1,11 +1,25 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
 
 export class CreateCoursesTable1671418046262 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'courses',
+        columns: [
+          { name: 'id', type: 'uuid', isPrimary: true },
+          { name: 'name', type: 'varchar', length: '10' },
+          { name: 'description', type: 'varchar', length: '100' },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'CURRENT_TIMESTAMP',
+          },
+        ],
+      }),
+    );
+  }
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-    }
-
-    public async down(queryRunner: QueryRunner): Promise<void> {
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('courses');
+  }
 }
